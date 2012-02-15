c4b32809731356fb583f0b061584717abb9a0d24
rlemon <rob.lemon@gmail.com>
2012-02-15 17:02:19 -0500

    added CTRL+D base, and fixed TAB range selection

--- js/keycommands.js
+++ js/keycommands.js
@@ -31,16 +31,28 @@ var handlers = {
 	 * 		Insert a "\t" before each line in the selection, if no line exists insert "\t" at beginning of range
 	 * */
 	tab: function (e) {
-		var jsc = getElm(), selection = get_textarea_selection(jsc), sel_start = jsc.selectionStart, sel_end = jsc.selectionEnd, len = jsc.value.length, TAB = '\t', pos_start = 0, pos_end = 0;
+		var jsc = getElm(), selection = get_textarea_selection(jsc), sel_start = jsc.selectionStart, sel_end = jsc.selectionEnd, len = jsc.value.length, pos_start = 0, pos_end = 0;
 		if( selection ) {
-			// cheapy solution, does not work.
-			jsc.value = jsc.value.substring(0, sel_start) + selection.replace(/\n/g,'\n\t') + jsc.value.substring(sel_end, len);
+			// cheapy solution, but works.. so far.
+			jsc.value = '\t' + jsc.value.substring(0, sel_start) + selection.replace(/\n/g,'\n\t') + jsc.value.substring(sel_end, len);
+			pos_start = sel_start, pos_end = sel_end + selection.indexOf('\n') + 1;
 		} else {
-			jsc.value = jsc.value.substring(0, sel_start) + TAB + jsc.value.substring(sel_start, len);
+			jsc.value = jsc.value.substring(0, sel_start) + '\t' + jsc.value.substring(sel_start, len);
 			pos_start = pos_end = sel_start + 1;
 		}
 		jsc.setSelectionRange(pos_start, pos_end);
-	}
+	},
+	/* CTRL + D
+	 * No Selection
+	 * 		Duplicate the current line and insert to a new line directly following itself. Do not move the caret.
+	 * Selection
+	 * 		Duplicate the entire selection and append to itself. No not insert a new line, do not move the caret.
+	 *  */
+	 ctrl_d: function (e) {
+		 // do something
+		 console.log('I did something');
+	 }
 };
 	
 shortcut.add("Tab", handlers.tab);
+shortcut.add("Ctrl+D", handlers.ctrl_d);
